47822776b5d056cd3c256fe86e9eea2192693ec5
Al Oletres <alejandrooletres@gmail.com>
2025-02-05 20:08:07 +0800

    Update email configurations

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import express, { Request, Response } from "express";
 import { createTransport, SendMailOptions, Transporter } from "nodemailer";
+import cors from "cors";
+import { config } from "dotenv";
+config();
 
 const app = express();
 const PORT = 3000;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//
+
+const EMAIL_USER = process.env.EMAIL_USER;
+const EMAIL_PASS = process.env.EMAIL_PASS;
 
 const transporter: Transporter = createTransport({
   service: "gmail",
   auth: {
-    user: "lodionglower@gmail.com",
-    pass: "sqzadnavkornwehv",
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
   },
 });
 
 const sendMail = async ({ to, text }: SendMailOptions) => {
   const options: SendMailOptions = {
-    from: "lodionglower@gmail.com",
+    from: EMAIL_USER,
     to,
     subject: "Brgy. Lower Lodiong Notification",
     text,
@@ -24,11 +35,15 @@ const sendMail = async ({ to, text }: SendMailOptions) => {
 };
 
 app.post("/api/email", async (req: Request, res: Response) => {
-  const { to, text }: { to: string; text: string } = req.body;
+  try {
+    const { to, text }: { to: string; text: string } = req.body;
 
-  await sendMail({ to, text });
+    await sendMail({ to, text });
 
-  res.json({ message: "Email sent successfully" });
+    res.json({ message: "Email sent successfully" });
+  } catch (err) {
+    console.log("err", err);
+  }
 });
 
 app.listen(PORT, () => {
